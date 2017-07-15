ce7f58e02dcf28d2bc7d4c194ebfcd26a9b3a174
Rudolf Biczok <rcbiczok@gmail.com>
2017-07-15 18:19:40 +0200

    Transform conventional unit test into google tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,38 +1,7 @@
-#include <data_structure/graph.h>
-#include <data_structure/io/graph_io.h>
+#include "gtest/gtest.h"
 
-#include <iostream>
-
-int main(int argc, const char* argv[]) {
-    graph_access G;
-    std::string graph_filename = argv[1];
-
-    graph_io::readGraphWeighted(G, graph_filename);
-
-    size_t num_nodes = 0;
-    for(NodeID n = 0; n < G.number_of_nodes(); ++n) {
-        ++num_nodes;
-    }
-
-    size_t num_edges = 0;
-    for(NodeID n = 0; n < G.number_of_nodes(); ++n) {
-        for (auto neighbour : G.neighbours(n)) {
-            ++num_edges;
-        }
-    }
-
-    if (num_edges != G.number_of_edges()) {
-        std::cout << "Error!" << std::endl;
-    }
-
-    num_edges = 0;
-    for(NodeID n = 0; n < G.number_of_nodes(); ++n) {
-        num_edges += G.getNodeDegree(n);
-    }
-
-    if (num_edges != G.number_of_edges()) {
-        std::cout << "Error!" << std::endl;
-    }
-
-    std::cout << "Max degree = " << G.getMaxDegree() << std::endl;
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
