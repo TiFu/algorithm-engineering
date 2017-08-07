6536518b43886629e7a321bea65673e53fa2310c
Rudolf Biczok <rcbiczok@gmail.com>
2017-08-07 13:39:19 +0200

    Refactor micro benchmark

--- micro_benchs/colouring/hca_mb.cpp
+++ micro_benchs/colouring/hca_mb.cpp
@@ -6,51 +6,44 @@
 
 using namespace graph_colouring;
 
-void BM_sequential(benchmark::State &state,
-                   const char *graphFile) {
+
+void BM_hca(benchmark::State &state,
+            const char *graphFile) {
     while (state.KeepRunning()) {
         graph_access G;
         graph_io::readGraphWeighted(G, graphFile);
 
-        const size_t min_k = 7;
-        const size_t k = 9;
-        const size_t population_size = 100;
-        const size_t maxItr = 20;
-        const size_t L = 5;
-        const size_t A = 2;
-        const double alpha = 0.6;
-        auto result = hybridColouringAlgorithm(G, k, population_size, maxItr, L, A, alpha, 1);
-        if(graph_colouring::colorCount(result) > min_k || graph_colouring::numberOfConflictingEdges(G, result) > 2) {
-            std::cerr << "Should return a colouring with k=" << min_k << "\n";
+        auto threadCount = size_t(state.range(0));
+        auto min_k = size_t(state.range(1));
+        auto k = size_t(state.range(2));
+        auto population_size = size_t(state.range(3));
+        auto maxItr = size_t(state.range(4));
+        auto L = size_t(state.range(5));
+        auto A = size_t(state.range(6));
+        const double alpha = double(state.range(7)) / 10;
+        auto result = hybridColouringAlgorithm(G, k, population_size, maxItr, L, A, alpha, threadCount);
+        auto result_k = graph_colouring::colorCount(result);
+        if (result_k > min_k) {
+            std::cerr << "Should return a colouring with k = "
+                      << min_k << " (is actually " << result_k << ")\n";
         }
     }
 }
 
-void BM_parallel(benchmark::State &state,
-                 const char *graphFile) {
-    while (state.KeepRunning()) {
-        graph_access G;
-        graph_io::readGraphWeighted(G, graphFile);
+BENCHMARK_CAPTURE(BM_hca, miles250,
+                  "../../input/miles250-sorted.graph")
+        ->Unit(benchmark::kMillisecond)
+        ->Args({1, 7, 9, 100,  20, 5, 2, 6})
+        ->Args({2, 7, 9, 100,  20, 5, 2, 6})
+        ->Args({1, 7, 9, 1000, 20, 5, 2, 6})
+        ->Args({2, 7, 9, 1000, 20, 5, 2, 6});
 
-        const size_t min_k = 7;
-        const size_t k = 9;
-        const size_t population_size = 100;
-        const size_t maxItr = 20;
-        const size_t L = 5;
-        const size_t A = 2;
-        const double alpha = 0.6;
-        auto result = hybridColouringAlgorithm(G, k, population_size, maxItr, L, A, alpha);
-        if(graph_colouring::colorCount(result) > min_k || graph_colouring::numberOfConflictingEdges(G, result) > 2) {
-            std::cerr << "Should return a colouring with k=" << min_k << "\n";
-        }
-    }
-}
-
-BENCHMARK_CAPTURE(BM_sequential, miles250,
-                  "../../input/miles250-sorted.graph")->Unit(benchmark::kMillisecond);
-
-BENCHMARK_CAPTURE(BM_parallel, miles250,
-                  "../../input/miles250-sorted.graph")->Unit(benchmark::kMillisecond);
-
+/*
+BENCHMARK_CAPTURE(BM_hca, DSJC250_5,
+                  "../../input/DSJC250.5-sorted.graph")
+        ->Unit(benchmark::kMillisecond)
+        ->Args({1, 28, 30, 100, 20, 250, 2, 6})
+        ->Args({2, 28, 30, 100, 20, 250, 2, 6});
+*/
 
 BENCHMARK_MAIN()
