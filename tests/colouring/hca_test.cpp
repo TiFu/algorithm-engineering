b98c665f2a5bcb9a4b804309954e6a06f65c8d94
Rudolf Biczok <rcbiczok@gmail.com>
2017-08-07 15:30:09 +0200

    Refactor tests

--- tests/colouring/hca_test.cpp
+++ tests/colouring/hca_test.cpp
@@ -13,79 +13,6 @@
 
 using namespace graph_colouring;
 
-TEST(HybridColouringAlgorithm, miles250_Graph_random_selection) {
-    graph_access G;
-    std::string graph_filename = "../../input/miles250-sorted.graph";
-    graph_io::readGraphWeighted(G, graph_filename);
-
-    const size_t L = 5;
-    const size_t A = 2;
-    const double alpha = 0.6;
-    const size_t k = 7;
-    const size_t population_size = 20;
-    const size_t maxItr = 100;
-
-    std::vector<InitOperator> hcaInitOps = {[L, A, alpha](const graph_access &graph,
-                                                          const size_t colors) {
-        return tabuSearchOperator(graph,
-                                  initByGreedySaturation(graph, colors),
-                                  L,
-                                  A,
-                                  alpha);
-
-    }};
-
-    size_t hcaCrossoverOp1Count = 0;
-    size_t hcaCrossoverOp2Count = 0;
-
-    std::vector<CrossoverOperator> hcaCrossoverOps = {
-            [&hcaCrossoverOp1Count](const graph_access &G_,
-                                    const Colouring &s1,
-                                    const Colouring &s2) {
-                hcaCrossoverOp1Count++;
-                return gpxCrossover(s1, s2);
-            }, [&hcaCrossoverOp2Count](const graph_access &G_,
-                                       const Colouring &s1,
-                                       const Colouring &s2) {
-                hcaCrossoverOp2Count++;
-                return gpxCrossover(s1, s2);
-            }};
-
-    std::vector<LSOperator> hcaLSOps = {[L, A, alpha](const graph_access &graph,
-                                                      const Colouring &s) {
-        return tabuSearchOperator(graph, s, L, A, alpha);
-    }};
-
-    auto invalidColoring = std::make_shared<InvalidColouringStrategy>(hcaInitOps,
-                                                                      hcaCrossoverOps,
-                                                                      hcaLSOps);
-
-    colouringAlgorithm({invalidColoring},
-                       G,
-                       k,
-                       population_size,
-                       maxItr);
-    ASSERT_TRUE(hcaCrossoverOp1Count > 0 && hcaCrossoverOp1Count < maxItr * population_size / 2);
-    ASSERT_TRUE(hcaCrossoverOp2Count > 0 && hcaCrossoverOp2Count < maxItr * population_size / 2);
-}
-
-/*
-TEST(HybridColouringAlgorithm, miles250_Graph_k8) {
-    graph_access G;
-    std::string graph_filename = "../../input/miles250-sorted.graph";
-    graph_io::readGraphWeighted(G, graph_filename);
-
-    const size_t L = 5;
-    const size_t A = 2;
-    const double alpha = 0.6;
-    const size_t k = 8;
-    const size_t population_size = 20;
-    const size_t maxItr = 100;
-    auto s_best = graph_colouring::hybridColouringAlgorithm(G, k, population_size, maxItr, L, A, alpha);
-    EXPECT_EQ(graph_colouring::numberOfConflictingEdges(G, s_best), 2);
-}
- */
-
 TEST(HybridColouringAlgorithm, miles250_Graph_k7) {
     graph_access G;
     std::string graph_filename = "../../input/miles250-sorted.graph";
@@ -98,5 +25,6 @@ TEST(HybridColouringAlgorithm, miles250_Graph_k7) {
     const size_t population_size = 20;
     const size_t maxItr = 20;
     auto s_best = graph_colouring::hybridColouringAlgorithm(G, k, population_size, maxItr, L, A, alpha);
-    EXPECT_EQ(graph_colouring::numberOfConflictingEdges(G, s_best), 2);
+    EXPECT_EQ(graph_colouring::colorCount(s_best), 7);
+    EXPECT_TRUE(graph_colouring::numberOfConflictingEdges(G, s_best) <= 4);
 }
