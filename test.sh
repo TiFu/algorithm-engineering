6b94a15ccf5ba76667eb44a67d837d65917dc945
Rudolf Biczok <rcbiczok@gmail.com>
2017-08-08 15:28:25 +0200

    Update build script

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # This is what is run on the benchmark machine when you push to the repository
 mkdir build
-cd build; cmake -DCMAKE_BUILD_TYPE=Release ..;make;cd ..
+cd build;
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j 8
+cd ..
 mkdir results
-cd build/micro_benchs;
-pwd
+cd build/micro_benchs
 ./hca_mb > ../../results/hca_mb.txt
